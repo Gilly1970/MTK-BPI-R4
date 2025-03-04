8b83604faff7c69f32dabb5556cef91c05d8426f
Petr Wozniak <156495286+woziwrt@users.noreply.github.com>
2025-03-04 16:15:47 +0100

    patches refactor

--- build-mlo-beta3.sh
+++ build-mlo-beta3.sh
@@ -7,12 +7,14 @@ git clone --branch openwrt-24.10 https://git.openwrt.org/openwrt/openwrt.git ope
 cd openwrt; git checkout 315facfce6dc13d6ec1993db1e16532cadcfcaaa; cd -;	
 
 git clone --branch master https://git01.mediatek.com/openwrt/feeds/mtk-openwrt-feeds || true
-cd mtk-openwrt-feeds; git checkout 612001dcebc0385f0cfe5cc5ccbf5dfd640dd4e1; cd -;	
+#cd mtk-openwrt-feeds; git checkout 612001dcebc0385f0cfe5cc5ccbf5dfd640dd4e1; cd -;
+#cd mtk-openwrt-feeds; git checkout d1340b5dd0b879fb66b599c0dbb70b41d4f2d02e; cd -;	#LRO
+cd mtk-openwrt-feeds; git checkout 2d24500219727bf7279fdb2d8c06dc2fc74cc5eb; cd -;	#refactor openwrt patches according to SDK rules	
 
-# mtk autobuild rules modification - disable their gerrit for internal builds
-\cp -r my_files/rules mtk-openwrt-feeds/autobuild/unified
+# mtk autobuild rules modification - disable their gerrit
+#\cp -r my_files/rules mtk-openwrt-feeds/autobuild/unified
 
-# wireless-regdb modificalion
+# wireless-regdb modification
 rm -rf openwrt/package/firmware/wireless-regdb/patches/*.*
 rm -rf mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches/*.*
 \cp -r my_files/500-tx_power.patch mtk-openwrt-feeds/autobuild/unified/filogic/mac80211/24.10/files/package/firmware/wireless-regdb/patches
@@ -35,7 +37,7 @@ sed -i 's/CONFIG_PACKAGE_perf=y/# CONFIG_PACKAGE_perf is not set/' mtk-openwrt-f
 cd openwrt
 bash ../mtk-openwrt-feeds/autobuild/unified/autobuild.sh filogic-mac80211-bpi-r4 log_file=make
 
-#exit 1
+#exit 0
 
 # thermal zone addition
 \cp -r my_files/w-mt7988a.dtsi openwrt/target/linux/mediatek/files-6.6/arch/arm64/boot/dts/mediatek/mt7988a.dtsi
